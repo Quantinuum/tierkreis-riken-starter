d93a891f08d079b6fb02e620fb970fac7e43281c
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-12 13:15:23 +0000

    Add reimei example

--- graphs/compile_run_reimei.py
+++ graphs/compile_run_reimei.py
@@ -7,18 +7,17 @@ from tierkreis.executor import UvExecutor
 from tierkreis.storage import FileStorage, read_outputs  # type: ignore
 
 from data import RIKEN_WORKERS_DIR, deterministic
-from workers.ibm_kobe.stubs import get_transpile_info, compile_using_info, submit
+from workers.reimei.stubs import compile_offline, sqcsub_submit_circuit
 
 Circuit = OpaqueType["pytket._tket.circuit.Circuit"]
 BackendResult = OpaqueType["pytket.backends.backendresult.BackendResult"]
 g = GraphBuilder(TKR[Circuit], TKR[dict[str, list[str]]])
-info = g.task(get_transpile_info())
-compiled_circuit = g.task(compile_using_info(info.config, info.props, g.inputs))
-res = g.task(submit(compiled_circuit, g.const(10)))
+compiled_circuit = g.task(compile_offline(g.inputs))
+res = g.task(sqcsub_submit_circuit(compiled_circuit, g.const(10)))
 g.outputs(res)
 
 if __name__ == "__main__":
-    storage = FileStorage(UUID(int=400), do_cleanup=True)
+    storage = FileStorage(UUID(int=401), do_cleanup=True)
     env = {"IS_DEV": "True"} if len(argv) > 1 and argv[1] == "dev" else {}
     exec = UvExecutor(RIKEN_WORKERS_DIR, storage.logs_path, env=env)
     run_graph(storage, exec, g, deterministic(), polling_interval_seconds=1)
