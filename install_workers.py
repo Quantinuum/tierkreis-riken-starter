9d76acd39813dc7df7a90f4dc8a5a71cc696b4de
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-13 10:57:04 +0000

    Use TKR pkg

--- install_workers.py
+++ install_workers.py
@@ -1,54 +1,9 @@
-from os import symlink
 from pathlib import Path
-import subprocess
-from typing import Literal
+from tierkreis.pkg import install_dependencies
+from tierkreis.pkg.base import TKRDependency
+from tierkreis.pkg.github import GitHubDependency
 
-from pydantic import BaseModel
-
-WORKER_CACHE = Path.home() / ".tierkreis" / "workers" / "github"
-
-
-class GitHubDependency(BaseModel):
-    type: Literal["github"] = "github"
-
-    account: str
-    repo: str
-    subdirectory: str
-
-    branch: str = "main"
-
-    def install(self, worker_name: str, target_dir: Path):
-        cache_dir = WORKER_CACHE / self.account / self.repo
-        cache_dir.mkdir(exist_ok=True, parents=True)
-
-        git_dir = cache_dir / ".git"
-        git_url = f"https://github.com/{self.account}/{self.repo}"
-        if not git_dir.exists():
-            subprocess.run(["git", "clone", git_url, "."], cwd=cache_dir)
-
-        subprocess.run(["git", "checkout", self.branch], cwd=cache_dir)
-        subprocess.run(["git", "pull"], cwd=cache_dir)
-
-        worker_dir = target_dir / worker_name
-        if worker_dir.exists():
-            worker_dir.unlink()
-
-        symlink(cache_dir / self.subdirectory, worker_dir, target_is_directory=True)
-
-
-class PathDependency(BaseModel):
-    type: Literal["path"] = "path"
-
-    worker_dir: Path
-
-    def install(self, worker_name: str, target_dir: Path):
-        """Not implemented..."""
-        return
-
-
-TKRDependency = GitHubDependency | PathDependency
-
-deps = {
+deps: dict[str, TKRDependency] = {
     "tkr_ibm_kobe": GitHubDependency(
         account="quantinuum",
         repo="tierkreis-riken",
@@ -78,11 +33,5 @@ deps = {
     ),
 }
 
-
-def install_workers(deps: dict[str, GitHubDependency], target_dir: Path):
-    for worker_name, dep in deps.items():
-        dep.install(worker_name, target_dir)
-
-
 if __name__ == "__main__":
-    install_workers(deps=deps, target_dir=Path.cwd() / "workers")
+    install_dependencies(deps=deps, target_dir=Path.cwd() / "workers")
