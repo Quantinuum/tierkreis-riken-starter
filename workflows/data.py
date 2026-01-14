95de8e2b26cb2110994f6e8984699ded7807bd83
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-14 16:02:15 +0000

    Remove extra circuit def

--- workflows/data.py
+++ workflows/data.py
@@ -4,14 +4,6 @@ from pytket._tket.circuit import Circuit
 RIKEN_WORKERS_DIR = Path(__file__).parent / ".." / "workers"
 
 
-def ghz() -> Circuit:
-    circ1 = Circuit(2)
-    circ1.H(0)
-    circ1.CX(0, 1)
-    circ1.measure_all()
-    return circ1
-
-
 def deterministic() -> Circuit:
     circ = Circuit(2, 2)
     circ.X(1).measure_all()
