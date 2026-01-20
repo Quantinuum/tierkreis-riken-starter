7f201318c59ba9d2d72bca6b552925c5737a84c2
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-20 13:34:24 +0000

    Do not move IBM token into place

--- scripts/test_switcher.sh
+++ scripts/test_switcher.sh
@@ -23,7 +23,7 @@ sqcsub --nqubits 10 \
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
 cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-ibm-kobe-dacc.token
-mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
+# mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
 sqcsub --nqubits 10 \
