b0c7aac40a38a321d910e9b30830ab1f7ba3d1d0
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-20 09:29:51 +0000

    Separate tokens in smoke tests

--- tests/smoke_tests/test_ibm_kobe.sh
+++ tests/smoke_tests/test_ibm_kobe.sh
@@ -3,6 +3,7 @@ DIR=$(dirname $0)
 . /vol0300/share/ra010014/jhpcq/x86/scripts/install-cert-files.sh ibm-kobe-dacc;
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
+cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-ibm-kobe-dacc.token
 mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh ibm-kobe-dacc;
--- tests/smoke_tests/test_reimei_simulator.sh
+++ tests/smoke_tests/test_reimei_simulator.sh
@@ -3,6 +3,7 @@ DIR=$(dirname $0)
 . /vol0300/share/ra010014/jhpcq/x86/scripts/install-cert-files.sh reimei;
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
+cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-reimei-simulator.token
 mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
