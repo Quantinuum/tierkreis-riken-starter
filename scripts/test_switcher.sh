e4b4e58f59299b3ce455289977fd0de4a0f5054b
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-20 13:37:16 +0000

    Less logging

--- scripts/test_switcher.sh
+++ scripts/test_switcher.sh
@@ -1,13 +1,15 @@
-set -euxo pipefail
+set -euo pipefail
 
 DIR=$(dirname $0)
 
+echo "Auth reimei-simulator"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/install-cert-files.sh reimei-simulator;
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
 cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-reimei-simulator.token
 mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
+echo "Run reimei-simulator"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
 sqcsub --nqubits 10 \
     --nshots 30 \
@@ -18,13 +20,14 @@ sqcsub --nqubits 10 \
     --qpu reimei-simulator
 
 
-
+echo "Auth IBM Kobe"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/install-cert-files.sh ibm-kobe-dacc;
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
 cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-ibm-kobe-dacc.token
 # mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
+echo "Run reimei-simulator"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
 sqcsub --nqubits 10 \
     --nshots 30 \
