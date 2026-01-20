e431f887f207e047182dbeea5a78b8df446bba4d
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-20 13:38:54 +0000

    Remove comment

--- scripts/test_switcher.sh
+++ scripts/test_switcher.sh
@@ -25,7 +25,6 @@ echo "Auth IBM Kobe"
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
 cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-ibm-kobe-dacc.token
-# mv $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt.token
 
 echo "Run reimei-simulator"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
