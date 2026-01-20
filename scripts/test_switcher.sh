9ac1f8bd1201fea96addb819b8eef532aeeb47a3
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-20 13:41:36 +0000

    Hunch is wrong

--- scripts/test_switcher.sh
+++ scripts/test_switcher.sh
@@ -25,7 +25,6 @@ echo "Auth IBM Kobe"
 source /vol0003/share/ra010014/jhpcq/bin/jhpc-q-setup.sh
 fetch_qtm_jwt.py
 cp $HOME/.qtm.jwt $HOME/.sqc_rpc_sched/jwt-ibm-kobe-dacc.token
-cp $HOME/.sqc_rpc_sched/jwt-reimei-simulator.token $HOME/.qtm.jwt 
 
 echo "Run reimei-simulator"
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh reimei-simulator
